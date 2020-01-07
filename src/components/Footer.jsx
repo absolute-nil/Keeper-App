9ff9e69caef6d78f81cb14115a505ee3ba5d09f8
Your Name <you@example.com>
2020-01-07 21:37:16 +0530

    create components of footer,note and header

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+let date = new Date();
+let year = date.getFullYear();
+function Footer() {
+  return (
+    <footer>
+      <p>copyright {year}</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1>New Note</h1>
+      <p>enter what you want to write</p>
+    </div>
+  );
+}
+
+export default Note;
